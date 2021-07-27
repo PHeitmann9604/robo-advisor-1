dedf89a6c31f8a7c3e3305ba0f179ec001af5060
PHeitmann9604 <pjh9604@stern.nyu.edu>
2021-07-27 09:50:28 -0400

    updated csv file with different prices

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -48,19 +48,26 @@ recent_low = min(low_prices)
 
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
+csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
 with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader()
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
+    for date in dates:
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["6. volume"]
+        })
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-# code for datetikme = https://www.geeksforgeeks.org/get-current-date-using-python/
+# code for datetime = https://www.geeksforgeeks.org/get-current-date-using-python/
 
 print(f"REQUEST AT: {datetime.now()}")
 print("-------------------------")
